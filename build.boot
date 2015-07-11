a1ee18c97fe06c0343e653fe6bdc433ecb884a4d
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-10 21:46:37 -0500

    Switch to using midje for testing

--- build.boot
+++ build.boot
@@ -1,17 +1,18 @@
 (set-env!
  :source-paths #{"src" "test"}
  :resource-paths #{"src"}
- :dependencies '[[org.clojure/clojure         "1.6.0"]
-                 [adzerk/boot-test            "1.0.4"]
-                 [radicalzephyr/clansi        "1.2.0"]
-                 [clj-stacktrace              "0.2.8"]
-                 [junit                       "4.12"]
+ :dependencies '[[org.clojure/clojure  "1.6.0"]
+                 [midje                "1.7.0"          :scope "test"]
+                 [zilti/boot-midje     "0.2.1-SNAPSHOT" :scope "test"]
+                 [radicalzephyr/clansi "1.2.0"]
+                 [clj-stacktrace       "0.2.8"]
+                 [junit                "4.12" ]
                  [org.reflections/reflections "0.9.10"]
                  [org.glassfish/javax.servlet "3.0"]
                  [radicalzephyr/bootlaces     "0.1.12"]])
 
 (require '[radicalzephyr.bootlaces :refer :all]
-         '[adzerk.boot-test])
+         '[zilti.boot-midje :refer [midje]])
 
 (def +version+ "0.1.1-SNAPSHOT")
 
@@ -24,9 +25,4 @@
        :url         "https://github.com/radicalzephyr/cljunit"
        :scm         {:url "https://github.com/radicalzephyr/cljunit"}
        :license     {"Eclipse Public License"
-                     "http://www.eclipse.org/legal/epl-v10.html"}}
- adzerk.boot-test/test {:namespaces '[cljunit.core-test]})
-
-(deftask test []
-  (comp (javac)
-        (adzerk.boot-test/test)))
+                     "http://www.eclipse.org/legal/epl-v10.html"}})
--- test/cljunit/core_test.clj
+++ test/cljunit/core_test.clj
@@ -1,22 +1,22 @@
 (ns cljunit.core-test
   (:require [cljunit.core :refer :all]
-            [clojure.test :refer :all]))
+            [midje.sweet  :refer :all]))
 
-(deftest running-tests
- (testing "get-class-in-package"
-   (is (= nil          (get-class-in-package "java.io" "TestingClass")))
-   (is (= java.io.File (get-class-in-package "java.io" "File"))))
+(facts "about running tests"
+  (facts "about get-class-in-package"
+    (get-class-in-package "java.io" "TestingClass") => nil
+    (get-class-in-package "java.io" "File")         => java.io.File)
 
- (testing "get-suite-class"
-   (is (= nil          (find-class-in-packages ["java.io"] "TestingClass")))
-   (is (= java.io.File (find-class-in-packages ["java.io"] "File")))
-   (is (= java.io.File (find-class-in-packages ["java.net" "java.io"] "File"))))
+  (facts "about find-class-in-packages"
+    (find-class-in-packages ["java.io"] "TestingClass") => nil
+    (find-class-in-packages ["java.io"] "File") => java.io.File
+    (find-class-in-packages ["java.net" "java.io"] "File") => java.io.File)
 
- (testing "run-test-suite"
-   (with-out-str
-     (is (= nil (run-test-suite ["java.net"] "TestingClass")))
-     (is (= {:failures 0} (run-test-suite ["cljunit"] "CljUnitPassingSuite")))))
+  (facts "about run-test-suite"
+    (with-out-str
+      (run-test-suite ["java.net"] "TestingClass") => nil
+      (run-test-suite ["cljunit"] "CljUnitPassingSuite") => {:failures 0}))
 
- (testing "run-tests-in-packages"
-   (with-out-str
-     (is (= {:failures 0} (run-tests-in-packages ["cljunit"]))))))
+  (facts "about run-tests-in-packages"
+    (with-out-str
+      (run-tests-in-packages ["cljunit"]) => {:failures 0})))
